485419364aa4442ab59a20ed9008d5f064273ab2
Said RAHHAL <saidbnrahhal@gmail.com>
2023-04-15 10:05:50 +0000

    adding conf

--- src/main/java/com/capgemini/pfe/M02WebClient.java
+++ src/main/java/com/capgemini/pfe/M02WebClient.java
@@ -1,10 +1,16 @@
 package com.capgemini.pfe;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan(basePackages = "com.capgemini.pfe")
 @SpringBootApplication
 public class M02WebClient  extends SpringBootServletInitializer{
 
