de303b573daabce35b379bbfa52674e8f82cc647
Said RAHHAL <saidbnrahhal@gmail.com>
2023-04-15 05:31:03 +0000

    Fix error in deployment

--- src/main/java/com/capgemini/pfe/M02WebClient.java
+++ src/main/java/com/capgemini/pfe/M02WebClient.java
@@ -2,12 +2,18 @@ package com.capgemini.pfe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class M02WebClient {
+public class M02WebClient  extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(M02WebClient.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(M02WebClient.class);
+	}
 }
