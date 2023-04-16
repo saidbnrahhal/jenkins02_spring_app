036eccaf97c8bce05595a6194590f6eb3f1df7a1
Said RAHHAL <saidbnrahhal@gmail.com>
2023-04-16 01:39:09 +0000

    make it more funny

--- src/main/java/com/capgemini/pfe/controller/UserController.java
+++ src/main/java/com/capgemini/pfe/controller/UserController.java
@@ -1,6 +1,9 @@
 package com.capgemini.pfe.controller;
 
+import com.capgemini.pfe.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,8 +11,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/")
 public class UserController {
 
+    @Autowired
+    private UserService userService;
+
     @GetMapping
-    public String getIndexPage(){
+    public String getIndexPage(Model model){
+        model.addAttribute("users", userService.findAllUsers());
         return "index";
     }
 
